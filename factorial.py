e12a45e143cd184b710ec7373f4e2e459dcae332
Rakesh Panda <rakesh.panda@intimetec.com>
2021-03-02 15:39:07 +0530

    command line

--- factorial.py
+++ factorial.py
@@ -1,7 +1,8 @@
+import sys
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
 n=int(input("Input a number to compute the factiorial : "))
-print(factorial(n))
+print(factorial(int(sys.argv[1])))
